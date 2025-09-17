213667a6acb3a6f5e91436eefe74067f499ed4b4
Giancarlo Carvajal Flor <nitroso1814@gmail.com>
2025-09-17 10:51:06 -0500

    fix: unused props in Grid

--- src/components/atoms/grid/Grid.tsx
+++ src/components/atoms/grid/Grid.tsx
@@ -3,7 +3,7 @@ import styles from './Grid.module.css';
 interface GridProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 function Grid({ children, className, ...props }: GridProps) {
-  return <div className={styles.grid + ' ' + className}>{children}</div>;
+  return <div className={styles.grid + ' ' + className} {...props}>{children}</div>;
 }
 
 export default Grid;
